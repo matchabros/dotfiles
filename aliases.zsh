c276c50f1cfbf2a22f37dfc28f50301ea2a5aa4e
achlab <investmch@gmail.com>
2017-06-24 17:39:14 +0100

    First commit of my new dotfiles

---
+++ aliases.zsh
@@ -0,0 +1,27 @@
+# Shortcuts
+# alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+# alias reloadcli="source $HOME/.zshrc"
+# alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+# alias ll="$(brew --prefix coreutils)/libexec/gnubin/ls -ahlF --color --group-directories-first"
+alias weather="curl -4 http://wttr.in"
+# alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
+alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
+
+# Directories
+alias dotfiles="cd $DOTFILES"
+# alias library="cd $HOME/Library"
+# alias sites="cd $HOME/Sites"
+
+#Open Sublime Text 3
+
+# alias subl="open -a Sublime\ Text.app"
+
+#Desktop navigation shortcuts
+
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+alias g="git"
+alias h="history"
+
+# Open zshconfig file
+alias zshconfig="subl ~/.zshrc"
---
+++ readme.md
@@ -0,0 +1,3 @@
+# My Dotfiles
+
+## Readme inc.
